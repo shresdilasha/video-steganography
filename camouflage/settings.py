a90bc6bccc0f6e7413b062f73b63c0d1262f911b
whyme-duh <ritikshrestha94@gmail.com>
2023-12-19 09:51:30 +0545

    changes made in settings file

--- camouflage/settings.py
+++ camouflage/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('KEY')
+SECRET_KEY = 'django-insecure-dp^$n284^shm&h!*=t&$a-cd$og4%u-$l6b-%36h))5m&y+p6('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
